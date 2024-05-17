1f4cd7e271fd4508c4d4b5f454238de5788a8523
nag-champa <68800824+nag-champa@users.noreply.github.com>
2024-05-17 15:07:50 -0400

    Update ansible-install-ansible.sh

--- ansible/ansible-install-ansible.sh
+++ ansible/ansible-install-ansible.sh
@@ -7,3 +7,6 @@ pipx install --include-deps ansible
 pipx ensurepath #set ansible path
 ssh-keygen -t ed25519 -N '' -f /home/ansible/.ssh/ansible_key #create ssh keys
 git clone https://github.com/nag-champa/homelab.git  #Git clone homelab code repo
+
+# Authenticate to github
+#gh auth login
